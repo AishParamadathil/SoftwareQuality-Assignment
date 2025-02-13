f960b1c6280efa7fb88305c336ed0f22a1c568fd
Aiswarya Paramadathil <107682605+AishParamadathil@users.noreply.github.com>
2025-02-12 22:41:39 -0500

    Test for a valid equilateral triangle

---
+++ TriangleSolverTest/TriangleSolverTest/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TriangleSolver;
+
+namespace TriangleSolverTest
+{
+    [TestFixture]
+    public class TriangleTests
+    {
+        [Test]
+        public void AnalyzeTriangle_Input3EqualSides_ReturnsEquilateral()
+        {
+            // Arrange
+            int firstSide = 5;
+            int secondSide = 5;
+            int thirdSide = 5;
+            string expected = "Equilateral triangle";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+
+
+        }
+    }
+}
