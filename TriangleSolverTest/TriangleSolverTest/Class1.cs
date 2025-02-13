094f378f311f296e534dc7c84d253df81935a7c9
Aiswarya Paramadathil <107682605+AishParamadathil@users.noreply.github.com>
2025-02-12 23:12:13 -0500

    three tests for valid isosceles triangle

--- TriangleSolverTest/TriangleSolverTest/Class1.cs
+++ TriangleSolverTest/TriangleSolverTest/Class1.cs
@@ -11,6 +11,7 @@ namespace TriangleSolverTest
     [TestFixture]
     public class TriangleTests
     {
+        //test for valid equilateral triangle
         [Test]
         public void AnalyzeTriangle_Input3EqualSides_ReturnsEquilateral()
         {
@@ -25,8 +26,55 @@ namespace TriangleSolverTest
 
             // Assert
             Assert.That(actual, Is.EqualTo(expected));
+        }
 
+        //tests for valid isosceles triangle
+        [Test]
+        public void AnalyzeTriangle_TwoEqualSidesAtBeginning_ReturnsIsosceles()
+        {
+            // Arrange
+            int firstSide = 5;
+            int secondSide = 5;
+            int thirdSide = 3;
+            string expected = "Isosceles triangle";
 
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void AnalyzeTriangle_TwoEqualSidesAtEnd_ReturnsIsosceles()
+        {
+            // Arrange
+            int firstSide = 4;
+            int secondSide = 6;
+            int thirdSide = 6;
+            string expected = "Isosceles triangle";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void AnalyzeTriangle_LargeIsoscelesTriangle_ReturnsIsosceles()
+        {
+            // Arrange
+            int firstSide = 10;
+            int secondSide = 10;
+            int thirdSide = 15;
+            string expected = "Isosceles triangle";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
         }
     }
 }
